fbd59497d3e0939b36d34ad68dcefdb28f4b78cf
sazzadwasy <sazzad.wasy@gmail.com>
2022-05-27 10:42:38 +0600

    businesssummary and reviews section added

--- src/pages/Home/BusinessSummary.js
+++ src/pages/Home/BusinessSummary.js
@@ -3,6 +3,8 @@ import React from 'react';
 const BusinessSummary = () => {
     return (
         <div>
+            <h2 className='py-14 text-4xl text-center text-cyan-900 font-bold'>Business Summary</h2>
+
 
         </div>
     );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from './Banner';
+import BusinessSummary from './BusinessSummary';
 import Parts from './Parts';
+import Reviews from './Reviews';
 
 const Home = () => {
     return (
         <div className='px-12'>
             <Banner></Banner>
             <Parts></Parts>
+            <BusinessSummary></BusinessSummary>
+            <Reviews></Reviews>
         </div>
     );
 };
